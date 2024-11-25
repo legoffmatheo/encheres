59b728cf1f10a5f1ef7107852626714e99e90dd2
Matheo <legoff.matheo4@gmail.com>
2024-11-25 10:14:44 +0100

    ok

---
+++ assets/js/compenents/Bonjour.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="conteur">
+    <h1>{{mot}}</h1>
+    <button @click="afficher">Afficher</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Bonjour',
+  setup() {
+
+  const mot = ref ('');
+
+  const afficher =(()=>{
+    mot.value = "Bonjour";
+  })
+  return{
+    mot,
+    afficher,
+  }
+
+  }
+  };
+</script>
+
+<style scoped>
+
+</style>
